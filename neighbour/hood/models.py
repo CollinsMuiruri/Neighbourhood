3d08d05d31f0f57fa41021a7a8fa0985206a7389
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-26 18:05:41 +0300

    Create Model classes

--- neighbour/hood/models.py
+++ neighbour/hood/models.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Admin(models.Model):
+    admin_name = models.CharField(max_length=50)
+
+
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.CharField(max_length=100)
+    neighbourhood_location = models.CharField(max_length=50)
+    occuupants_count = models.CharField(max_length=500)
+    admin = models.ForeignKey(Admin)
+
+
+class UserProfileModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=50)
+    # user_id = models.AutoField(primary_key=True)
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    email = models.EmailField(max_length=80)
+
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=100)
+    user = models.ForeignKey(User)
+    neighbourhood = models.ForeignKey(Neighbourhood)
+    email = models.EmailField(max_length=80)
