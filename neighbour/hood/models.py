bf6625e19c33ed05466f5db116eb08847af11246
Collins Muiruri <wanyekicollins@gmail.com>
2018-05-29 19:32:57 +0300

    Add Group models and templates

--- neighbour/hood/models.py
+++ neighbour/hood/models.py
@@ -171,3 +171,41 @@ class Comment(models.Model):
     def get_comment(cls):
         comment = Comment.objects.all()
         return comment
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(default='', blank=True)
+    description_html = models.TextField(editable=False, default='', blank=True)
+    members = models.ManyToManyField(User, through='GroupMember')
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('groups:single', kwargs={'slug': self.slug})
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        name = cls.objects.filter(name__icontains=search_term)
+        return name
+
+    class Meta:
+        ordering = ['name']
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group, related_name='memberships')
+    user = models.ForeignKey(User, related_name='user_groups')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        unique_together = ('group', 'user')
